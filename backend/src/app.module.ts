df4dcbe80d33dd3fe44c8b01a6b0007ffe6d27cb
Quốc Chương <quocchuong2609@gmail.com>
2024-12-19 10:12:17 +0700

    update: setup base backend

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,10 +1,26 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { MiddlewareConsumer, Module, NestModule } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { MongooseModule } from '@nestjs/mongoose';
+
+import corsConfig from '@/config/cors.config';
+import { DomainsModule } from '@/domains/domains.module';
+import { MorganMiddleware } from '@/middlewares/morgan.middleware';
 
 @Module({
-	imports: [],
-	controllers: [AppController],
-	providers: [AppService],
+	imports: [
+		ConfigModule.forRoot({ load: [corsConfig], isGlobal: true }),
+		MongooseModule.forRootAsync({
+			imports: [ConfigModule],
+			useFactory: async (configService: ConfigService) => ({
+				uri: configService.get<string>('DATABASE_URL'),
+			}),
+			inject: [ConfigService],
+		}),
+		DomainsModule,
+	],
 })
-export class AppModule {}
+export class AppModule implements NestModule {
+	configure(consumer: MiddlewareConsumer) {
+		consumer.apply(MorganMiddleware).forRoutes('*');
+	}
+}
