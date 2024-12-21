c431200199eb21b8262fb119bba86c815e040ca6
Quốc Chương <quocchuong2609@gmail.com>
2024-12-21 13:37:40 +0700

    add: implement `payment` schema

---
+++ backend/src/schemas/payment.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Payment {
+	@Prop({ type: Types.ObjectId, ref: 'Voucher', required: true })
+	voucher: Types.ObjectId;
+
+	@Prop({ required: true })
+	method: 'momo' | 'zalo-pay' | 'vn-pay';
+
+	@Prop({ required: true })
+	originPrice: number;
+
+	@Prop({ required: true })
+	finalPrice: number;
+}
+
+export const PaymentSchema = SchemaFactory.createForClass(Payment);
