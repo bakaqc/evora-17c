e225a31fecf7b255ea30211704295ef4f818d5ff
Quốc Chương <quocchuong2609@gmail.com>
2024-12-21 13:44:35 +0700

    add: implement `review` schema

---
+++ backend/src/schemas/review.schema.ts
@@ -0,0 +1,19 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Review {
+	@Prop({ type: Types.ObjectId, ref: 'Event', required: true })
+	event: Types.ObjectId;
+
+	@Prop({ type: Types.ObjectId, ref: 'User', required: true })
+	user: Types.ObjectId;
+
+	@Prop({ required: true })
+	rating: number;
+
+	@Prop({ required: true })
+	comment: string;
+}
+
+export const ReviewSchema = SchemaFactory.createForClass(Review);
