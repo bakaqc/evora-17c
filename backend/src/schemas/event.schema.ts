d86342201b40edc4ec4eace5ea689e6ddda0cfe2
Quốc Chương <quocchuong2609@gmail.com>
2024-12-21 13:27:28 +0700

    add: implement `event` schema

---
+++ backend/src/schemas/event.schema.ts
@@ -0,0 +1,45 @@
+import { Prop, Schema, SchemaFactory } from '@nestjs/mongoose';
+import { Types } from 'mongoose';
+
+@Schema({ timestamps: true })
+export class Booking {
+	@Prop({ type: Types.ObjectId, ref: 'User', required: true })
+	user: Types.ObjectId;
+
+	@Prop({ type: Types.ObjectId, ref: 'Category', required: true })
+	category: Types.ObjectId;
+
+	@Prop({ required: true })
+	title: string;
+
+	@Prop({ required: true })
+	description: string;
+
+	@Prop({
+		type: [
+			{
+				type: {
+					type: String,
+					enum: ['Basic', 'Premium', 'VIP'],
+					required: true,
+				},
+				price: { type: Number, required: true },
+			},
+		],
+	})
+	attributes: { type: string; price: number }[];
+
+	@Prop({ required: true })
+	photos: string[];
+
+	@Prop({ required: true })
+	ratingTotal: number;
+
+	@Prop({ required: true })
+	ratingCount: number;
+
+	@Prop({ default: false })
+	isDeleted: boolean;
+}
+
+export const BookingSchema = SchemaFactory.createForClass(Booking);
