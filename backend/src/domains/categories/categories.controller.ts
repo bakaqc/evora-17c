eccbe2a1bcb9447374fa4aaaeb28f7dea2210924
Quốc Chương <quocchuong2609@gmail.com>
2024-12-22 08:07:14 +0700

    update: implement api create `category`

--- backend/src/domains/categories/categories.controller.ts
+++ backend/src/domains/categories/categories.controller.ts
@@ -1,10 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
 import { CategoriesService } from '@/domains/categories/categories.service';
+import { CreateCategoryDto } from '@/domains/categories/dto/createCategory.dto';
 
 @ApiTags('Categories')
 @Controller('categories')
 export class CategoriesController {
 	constructor(private readonly categoriesService: CategoriesService) {}
+
+	@Post()
+	async create(@Body() CreateCategoryDto: CreateCategoryDto) {
+		return await this.categoriesService.create(CreateCategoryDto);
+	}
 }
--- backend/src/domains/categories/categories.service.ts
+++ backend/src/domains/categories/categories.service.ts
@@ -1,7 +1,8 @@
-import { Injectable, Logger } from '@nestjs/common';
+import { ConflictException, Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+import { CreateCategoryDto } from '@/domains/categories/dto/createCategory.dto';
 import { Category } from '@/schemas/category.schema';
 
 @Injectable()
@@ -11,4 +12,35 @@ export class CategoriesService {
 	constructor(
 		@InjectModel(Category.name) private readonly categoryModel: Model<Category>,
 	) {}
+
+	async create(createCategoryDto: CreateCategoryDto) {
+		const existingCategory = await this.categoryModel.findOne({
+			name: createCategoryDto.name,
+		});
+
+		if (existingCategory) {
+			this.logger.error(
+				`Category with name ${createCategoryDto.name} already exists!`,
+			);
+
+			throw new ConflictException('Category already exists');
+		}
+
+		const createdCategory = new this.categoryModel(createCategoryDto);
+
+		this.logger.debug(
+			`Creating category with name ${createdCategory.name}`,
+			createdCategory,
+		);
+
+		await createdCategory.save();
+
+		this.logger.log(`Category with name ${createdCategory.name} created`);
+
+		return {
+			success: true,
+			message: 'Category created successfully.',
+			data: createdCategory,
+		};
+	}
 }
