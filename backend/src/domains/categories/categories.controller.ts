0d75717060dc5974300262440c6f070b662185ac
Quốc Chương <quocchuong2609@gmail.com>
2024-12-22 12:05:46 +0700

    update: api operation for `category` & `user`

--- backend/src/domains/categories/categories.controller.ts
+++ backend/src/domains/categories/categories.controller.ts
@@ -7,7 +7,7 @@ import {
 	Post,
 	Put,
 } from '@nestjs/common';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiOperation, ApiTags } from '@nestjs/swagger';
 
 import { CategoriesService } from '@/domains/categories/categories.service';
 import { CreateCategoryDto } from '@/domains/categories/dto/createCategory.dto';
@@ -18,21 +18,25 @@ import { UpdateCategoryDto } from '@/domains/categories/dto/updateCategory.dto';
 export class CategoriesController {
 	constructor(private readonly categoriesService: CategoriesService) {}
 
+	@ApiOperation({ summary: 'Create a new category' })
 	@Post()
 	async create(@Body() CreateCategoryDto: CreateCategoryDto) {
 		return await this.categoriesService.create(CreateCategoryDto);
 	}
 
+	@ApiOperation({ summary: 'Fetch all categories' })
 	@Get()
 	async getAll() {
 		return await this.categoriesService.getAll();
 	}
 
+	@ApiOperation({ summary: 'Fetch a category by ID' })
 	@Get(':id')
 	async getOne(@Param('id') id: string) {
 		return await this.categoriesService.getOne(id);
 	}
 
+	@ApiOperation({ summary: 'Update a category by ID' })
 	@Put(':id')
 	async update(
 		@Param('id') id: string,
@@ -41,6 +45,7 @@ export class CategoriesController {
 		return await this.categoriesService.update(id, updateCategoryDto);
 	}
 
+	@ApiOperation({ summary: 'Delete a category by ID' })
 	@Delete(':id')
 	async delete(@Param('id') id: string) {
 		return await this.categoriesService.delete(id);
