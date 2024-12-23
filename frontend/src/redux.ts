30d4a3500b67dc57a4ce26e6564199585a01730d
theqi106 <phkhhulolmb1@gmail.com>
2024-12-23 13:27:24 +0700

    add: setup base reducer

---
+++ frontend/src/redux.ts
@@ -0,0 +1,15 @@
+import { createStore } from 'redux';
+import { Store } from 'redux';
+import { persistStore } from 'redux-persist';
+import { Persistor } from 'redux-persist/es/types';
+
+import rootReducer from '@/stores/reducers/rootReducer';
+
+const reduxStore = (): { store: Store; persistor: Persistor } => {
+	const store = createStore(rootReducer);
+	const persistor = persistStore(store);
+
+	return { store, persistor };
+};
+
+export default reduxStore;
