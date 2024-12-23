349cd9773d32091897ed2f1cabb0baf672cb065d
Quốc Chương <quocchuong2609@gmail.com>
2024-12-23 22:02:20 +0700

    update: api payment use zalopay & momo gateway

--- backend/src/domains/payments/payments.controller.ts
+++ backend/src/domains/payments/payments.controller.ts
@@ -1,10 +1,38 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, HttpCode, HttpStatus, Post } from '@nestjs/common';
 import { ApiTags } from '@nestjs/swagger';
 
+import { CreatePaymentDto } from '@/domains/payments/dto/createPayment.dto';
+import { MomoService } from '@/domains/payments/momo/momo.service';
 import { PaymentsService } from '@/domains/payments/payments.service';
+import { ZalopayService } from '@/domains/payments/zalopay/zalopay.service';
 
 @ApiTags('Payments')
 @Controller('payments')
 export class PaymentsController {
-	constructor(private readonly paymentsService: PaymentsService) {}
+	constructor(
+		private readonly paymentsService: PaymentsService,
+		private readonly zalopayService: ZalopayService,
+		private readonly momoService: MomoService,
+	) {}
+
+	@HttpCode(HttpStatus.OK)
+	@Post()
+	async create(@Body() createPaymentDto: CreatePaymentDto) {
+		const response = await this.paymentsService.create(createPaymentDto);
+		const { order_url } = response;
+
+		return { payment_url: order_url };
+	}
+
+	@HttpCode(HttpStatus.OK)
+	@Post('zalopay/callback')
+	async zalopayCallback(@Body() body: any) {
+		return await this.zalopayService.callback(body);
+	}
+
+	@HttpCode(HttpStatus.OK)
+	@Post('momo/callback')
+	async momoCallback(@Body() body: any) {
+		return await this.momoService.callback(body);
+	}
 }
--- backend/src/domains/payments/payments.service.ts
+++ backend/src/domains/payments/payments.service.ts
@@ -2,6 +2,9 @@ import { Injectable, Logger } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 
+import { CreatePaymentDto } from '@/domains/payments/dto/createPayment.dto';
+import { MomoService } from '@/domains/payments/momo/momo.service';
+import { ZalopayService } from '@/domains/payments/zalopay/zalopay.service';
 import { Payment } from '@/schemas/payment.schema';
 
 @Injectable()
@@ -10,5 +13,48 @@ export class PaymentsService {
 
 	constructor(
 		@InjectModel(Payment.name) private readonly paymentModel: Model<Payment>,
+		private readonly zalopayService: ZalopayService,
+		private readonly momoService: MomoService,
 	) {}
+
+	async create(createPaymentDto: CreatePaymentDto) {
+		const newPayment = new this.paymentModel(createPaymentDto);
+
+		this.logger.debug('Creating new payment');
+
+		this.logger.debug('Payment created', newPayment);
+		this.logger.log('Payment created');
+
+		// Add paymentId to Booking
+
+		await newPayment.save();
+
+		switch (createPaymentDto.method) {
+			case 'vn-pay': {
+				const result = await this.zalopayService.create({
+					id: newPayment._id.toString(),
+					amount: newPayment.finalPrice,
+				});
+
+				return { order_url: result.order_url };
+			}
+			case 'zalo-pay': {
+				const result = await this.zalopayService.create({
+					id: newPayment._id.toString(),
+					amount: newPayment.finalPrice,
+				});
+
+				return { order_url: result.order_url };
+			}
+
+			case 'momo': {
+				const result = await this.momoService.create({
+					id: newPayment._id.toString(),
+					amount: newPayment.finalPrice,
+				});
+
+				return { order_url: result.shortLink };
+			}
+		}
+	}
 }
