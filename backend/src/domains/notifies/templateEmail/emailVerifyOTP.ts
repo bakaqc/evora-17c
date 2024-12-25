b4529a2144eb8d58f3d41c41652747f132054624
Quốc Chương <quocchuong2609@gmail.com>
2024-12-25 21:08:53 +0700

    update: template email verify OTP

--- backend/src/domains/notifies/templateEmail/emailVerifyOTP.ts
+++ backend/src/domains/notifies/templateEmail/emailVerifyOTP.ts
@@ -3,6 +3,7 @@ export const emailVerifyOTP = `
         <p>Xin chào,</p>
         <p>Cảm ơn bạn đã đăng ký tài khoản tại <strong style="color: #FF7F00;">Evora</strong>. Vui lòng sử dụng mã OTP sau để xác minh tài khoản của bạn:</p>
         <h2 style="color: #FF7F00;">{{OTP_CODE}}</h2>
+				<p>Mã OTP của bạn sẽ hết hạn sau <strong>5 phút</strong>.</p>
         <p>Nếu bạn không yêu cầu mã này, vui lòng bỏ qua email này.</p>
     </div>
 `;
