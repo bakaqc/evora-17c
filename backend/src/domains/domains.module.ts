d58b287cfbe44e15da6a8b07f803c7482f2e1fad
Quốc Chương <quocchuong2609@gmail.com>
2024-12-26 14:39:19 +0700

    update: setup jwt auth & role guard for domain module

--- backend/src/domains/domains.module.ts
+++ backend/src/domains/domains.module.ts
@@ -1,6 +1,9 @@
 import { Module } from '@nestjs/common';
+import { APP_GUARD } from '@nestjs/core';
 
 import { AuthModule } from '@/domains/auth/auth.module';
+import { JwtAuthGuard } from '@/domains/auth/guards/jwt.guard';
+import { RolesGuard } from '@/domains/auth/guards/roles.guard';
 import { BookingsModule } from '@/domains/bookings/bookings.module';
 import { NotifiesModule } from '@/domains/notifies/notifies.module';
 import { PartiesModule } from '@/domains/parties/parties.module';
@@ -19,6 +22,16 @@ import { VouchersModule } from '@/domains/vouchers/vouchers.module';
 		ReviewsModule,
 		UsersModule,
 		VouchersModule,
+	],
+	providers: [
+		{
+			provide: APP_GUARD,
+			useClass: JwtAuthGuard,
+		},
+		{
+			provide: APP_GUARD,
+			useClass: RolesGuard,
+		},
 	],
 })
 export class DomainsModule {}
