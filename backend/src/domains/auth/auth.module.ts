52cb652851d12a01ce89cc35a80389504edc2ec0
Quốc Chương <quocchuong2609@gmail.com>
2024-12-26 14:28:21 +0700

    update: setup jwt module

--- backend/src/domains/auth/auth.module.ts
+++ backend/src/domains/auth/auth.module.ts
@@ -1,18 +1,34 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
 import { MongooseModule } from '@nestjs/mongoose';
 
 import { AuthController } from '@/domains/auth/auth.controller';
 import { AuthService } from '@/domains/auth/auth.service';
 import { OtpService } from '@/domains/auth/otp.service';
+import { JwtStrategy } from '@/domains/auth/strategies/jwt.strategy';
 import { NotifiesModule } from '@/domains/notifies/notifies.module';
+import { UsersModule } from '@/domains/users/users.module';
 import { UserSchema } from '@/schemas/user.schema';
 
 @Module({
 	imports: [
 		MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
 		NotifiesModule,
+		JwtModule.registerAsync({
+			imports: [ConfigModule],
+			useFactory: async (configService: ConfigService) => ({
+				secret: configService.get<string>('jwt.secret'),
+				signOptions: {
+					expiresIn: configService.get<string>('jwt.signOptions.expiresIn'),
+				},
+			}),
+			inject: [ConfigService],
+		}),
+		UsersModule,
 	],
 	controllers: [AuthController],
-	providers: [AuthService, OtpService],
+	providers: [AuthService, OtpService, JwtStrategy],
+	exports: [AuthService],
 })
 export class AuthModule {}
