497db6f1e773d9c3521a3fcae0a37de73e3beddf
theqi106 <phkhhulolmb1@gmail.com>
2025-01-03 14:49:14 +0700

    add: add Login and UI after login

--- frontend/src/containers/public/LoginUser.tsx
+++ frontend/src/containers/public/LoginUser.tsx
@@ -1,11 +1,40 @@
-import React from 'react';
-import { NavLink } from 'react-router-dom';
+import { useEffect, useState } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { NavLink, useNavigate } from 'react-router-dom';
 
 import logo1 from '@/assets/logo1.png';
-import { ButtonForLogin, InputField } from '@/components';
+import { ButtonForLogin, InputForLogin } from '@/components';
+import { AppDispatch } from '@/redux';
+import * as actions from '@/stores/actions';
+import { RootState } from '@/stores/reducers/rootReducer';
 import { path } from '@/ultils/constant';
+import { PayloadForLogin } from '@/ultils/type';
+import validate from '@/ultils/validateField';
+
+interface InvalidField {
+	name: string;
+	msg: string;
+}
 
 const LoginUser: React.FC = () => {
+	const { isLogin } = useSelector((state: RootState) => state.auth);
+	const navigate = useNavigate();
+	const dispatch: AppDispatch = useDispatch();
+	const [payload, setPayload] = useState<PayloadForLogin>({
+		email: '',
+		password: '',
+	});
+	const [invalidField, setInvalidField] = useState<InvalidField[]>([]);
+	useEffect(() => {
+		if (isLogin) {
+			navigate('/');
+		}
+	}, [isLogin]);
+	const handleSubmit = async () => {
+		const invalid = validate(payload, setInvalidField);
+		if (invalid === 0) dispatch(actions.login(payload));
+	};
+
 	return (
 		<section className="gradient-form h-screen bg-neutral-200 dark:bg-neutral-700 flex items-center justify-center">
 			<div className="container max-w-5xl p-6">
@@ -23,12 +52,28 @@ const LoginUser: React.FC = () => {
 									</div>
 
 									{/* Username and Password Fields */}
-									<InputField id="username" label="Email" type="text" />
-									<InputField id="password" label="Password" type="password" />
+									<InputForLogin
+										id="email"
+										label="Email"
+										type="text"
+										invalidField={invalidField}
+										value={payload.email}
+										setValue={setPayload}
+										setInvalidField={setInvalidField}
+									/>
 
+									<InputForLogin
+										id="password"
+										label="Password"
+										type="password"
+										invalidField={invalidField}
+										value={payload.password}
+										setValue={setPayload}
+										setInvalidField={setInvalidField}
+									/>
 									{/* Login Button */}
 									<div className="mb-6 text-center">
-										<ButtonForLogin label="Log in" />
+										<ButtonForLogin label="Log in" onClick={handleSubmit} />
 										<a
 											href="#!"
 											className="mt-2 inline-block text-sm text-primary"
