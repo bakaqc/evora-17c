8784b204cb60384e33a1f1bad7ea6c7b8fa83dbb
thanglp163 <165275095+thanglp163@users.noreply.github.com>
2025-01-03 22:01:36 +0700

    update: index.ts

--- frontend/src/containers/system/index.ts
+++ frontend/src/containers/system/index.ts
@@ -1,6 +1,2 @@
 export { default as AdminPage } from './AdminPage';
-export { default as ListUsers } from './ListUsers';
-// export { default as ListBookings } from './ListBookings';
-// export { default as ListParties } from './ListParties';
-export { default as ListPayments } from './ListPayments';
-// export { default as ListVouchers } from './ListVouchers';
+
