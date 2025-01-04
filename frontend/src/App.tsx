33cbdeb31262e674ec949cfe7fb2e33bc52a8e34
theqi106 <phkhhulolmb1@gmail.com>
2025-01-04 21:42:03 +0700

    update: config routes

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,41 +1,25 @@
 import React, { useEffect } from 'react';
 import { useSelector } from 'react-redux';
-import { Route, Routes } from 'react-router-dom';
 import { ToastContainer } from 'react-toastify';
 
-import {
-	Home,
-	HomePage,
-	LoginAdmin,
-	LoginUser,
-	RegisterUser,
-	VerifyOTP,
-} from '@/containers/public';
-import { AdminPage } from '@/containers/system';
+import AdminRoutes from '@/routes/admin/AdminRoutes';
+import UserRoutes from '@/routes/user/UserRoutes';
 import { RootState } from '@/stores/reducers/rootReducer';
-import { path } from '@/ultils/constant';
 
 const App: React.FC = () => {
 	const { isLogin } = useSelector((state: RootState) => state.auth);
+
 	useEffect(() => {
 		console.log(isLogin);
 	}, [isLogin]);
+
 	return (
 		<div className="overflow-hidden">
-			<Routes>
-				{/* Route for User */}
-				<Route path={path.HOME} element={<Home />}>
-					<Route path="*" element={<HomePage />} />
-				</Route>
-				<Route path={path.LOGIN_USER} element={<LoginUser />} />
-				<Route path={path.LOGIN_ADMIN} element={<LoginAdmin />} />
-				<Route path={path.REGISTER_USER} element={<RegisterUser />} />
-				<Route path={path.VERIFY_OTP} element={<VerifyOTP />} />
+			{/* User and Admin Routes */}
+			<UserRoutes />
+			<AdminRoutes />
 
-				{/* Route for Admin */}
-				<Route path={path.ADMINPAGE} element={<AdminPage />} />
-			</Routes>
-
+			{/* Toast Notification */}
 			<ToastContainer position="top-center" />
 		</div>
 	);
