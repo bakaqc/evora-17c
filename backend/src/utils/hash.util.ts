208e987abef23c5921e6b0545fd836ba6bef0312
Quốc Chương <quocchuong2609@gmail.com>
2025-01-05 14:23:46 +0700

    add: api reset password

--- backend/src/utils/hash.util.ts
+++ backend/src/utils/hash.util.ts
@@ -3,10 +3,3 @@ import * as argon2 from 'argon2';
 export const hash = async (plainPassword: string): Promise<string> => {
 	return await argon2.hash(plainPassword);
 };
-
-export const verify = async (
-	hashedPassword: string,
-	plainPassword: string,
-): Promise<boolean> => {
-	return await argon2.verify(hashedPassword, plainPassword);
-};
