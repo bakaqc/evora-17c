7920f13c90a56665f08d2a90299a1a840afc4276
theqi106 <phkhhulolmb1@gmail.com>
2025-01-13 20:29:28 +0700

    add: pagination

---
+++ frontend/src/components/PageNumber.tsx
@@ -0,0 +1,84 @@
+import React, { ReactNode, memo } from 'react';
+import {
+	createSearchParams,
+	useLocation,
+	useNavigate,
+	useSearchParams,
+} from 'react-router-dom';
+
+interface PageNumberProps {
+	text?: string;
+	currentPage?: number;
+	icon?: ReactNode;
+	setCurrentPage?: React.Dispatch<React.SetStateAction<number>>;
+}
+const notActive =
+	'w-[46px] h-[48px] flex justify-center items-center px-5 py-3 bg-white hover:bg-gray-300 rounded-md';
+const active =
+	'w-[46px] h-[48px] flex justify-center items-center px-5 py-3 bg-[#E88F2A] text-white rounded-md cursor-pointer';
+const PageNumber: React.FC<PageNumberProps> = ({
+	text = '1',
+	currentPage = 1,
+	setCurrentPage,
+	icon,
+}) => {
+	const navigate = useNavigate();
+	const location = useLocation();
+	const [paramsSearch] = useSearchParams();
+	const entries = paramsSearch.entries();
+
+	const append = (entries: IterableIterator<[string, string]>) => {
+		const params: [string, string][] = [];
+		paramsSearch.append('page', String(text ?? ''));
+		for (const entry of entries) {
+			params.push(entry);
+		}
+		const searchParamsObject: Record<string, string | string[]> = {};
+
+		params?.forEach((i) => {
+			if (
+				Object.keys(searchParamsObject)?.some(
+					(item) => item === i[0] && item !== 'page',
+				)
+			) {
+				const existingValue = searchParamsObject[i[0]];
+
+				if (Array.isArray(existingValue)) {
+					searchParamsObject[i[0]] = [...existingValue, i[1]];
+				} else {
+					searchParamsObject[i[0]] = [existingValue, i[1]];
+				}
+			} else {
+				searchParamsObject[i[0]] = i[1];
+			}
+		});
+
+		return searchParamsObject;
+	};
+
+	const handleChangePage = () => {
+		if (!(text === '...')) {
+			if (setCurrentPage) {
+				setCurrentPage(+text!);
+			}
+			navigate({
+				pathname: location?.pathname,
+				search: createSearchParams(append(entries)).toString(),
+			});
+		}
+	};
+	return (
+		<div
+			className={
+				+text === +currentPage
+					? active
+					: `${notActive} ${text === '...' ? 'cursor-text' : 'cursor-pointer'}`
+			}
+			onClick={handleChangePage}
+		>
+			{icon || text}
+		</div>
+	);
+};
+
+export default memo(PageNumber);
---
+++ frontend/src/stores/actions/party.ts
@@ -0,0 +1,73 @@
+import { Dispatch } from 'redux';
+
+import {
+	ApiResponse,
+	apiGetParties,
+	apiGetPartiesByCategory,
+} from '@/services/party';
+import { PartyQuery } from '@/utils/type';
+
+import { actionTypes } from './actionTypes';
+
+export const getParties = (query: PartyQuery) => {
+	return async (dispatch: Dispatch): Promise<void> => {
+		try {
+			const response: ApiResponse = await apiGetParties(query);
+			if (response.success) {
+				dispatch({
+					type: actionTypes.GET_PARTIES,
+					parties: response.data.parties,
+					pagination: response.data.pagination,
+				});
+			} else {
+				dispatch({
+					type: actionTypes.GET_PARTIES,
+					msg: response.message,
+				});
+			}
+		} catch (error) {
+			console.error('Error fetching parties:', error);
+			dispatch({
+				type: actionTypes.GET_PARTIES,
+				parties: null,
+				pagination: null,
+			});
+		}
+	};
+};
+export const getPartiesByCategory = ({
+	category,
+	...query
+}: {
+	category: string;
+	query: PartyQuery;
+}) => {
+	return async (dispatch: Dispatch): Promise<void> => {
+		try {
+			const response: ApiResponse = await apiGetPartiesByCategory({
+				category,
+				...query,
+			});
+			console.log(response);
+			if (response.success) {
+				dispatch({
+					type: actionTypes.GET_PARTIES_BY_CATEGORY,
+					parties: response.data.parties,
+					pagination: response.data.pagination,
+				});
+			} else {
+				dispatch({
+					type: actionTypes.GET_PARTIES_BY_CATEGORY,
+					msg: response.message,
+				});
+			}
+		} catch (error) {
+			console.error('Error fetching parties:', error);
+			dispatch({
+				type: actionTypes.GET_PARTIES_BY_CATEGORY,
+				parties: null,
+				pagination: null,
+			});
+		}
+	};
+};
