18b23f016a814ea2390b5b243f87933ef3c479d4
thanglp163 <lethangtty2018@gmail.com>
2025-02-13 10:31:33 +0700

    update: admin data

--- frontend/src/components/Navbar.tsx
+++ frontend/src/components/Navbar.tsx
@@ -15,7 +15,6 @@ const Navbar = () => {
 					className="w-[200px] p-2 bg-transparent outline-none"
 				/>
 			</div>
-
 			{/* ICONS AND USER */}
 			<div className="flex items-center gap-6 justify-end w-full">
 				{/* User Info */}
